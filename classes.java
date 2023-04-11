9b14833dfaec2ee49a17956928dd96b1c228ff1e
Abhidha Joshi <124055802+Abhidhajoshi@users.noreply.github.com>
2023-04-11 22:27:15 +0530

    Add files via upload

---
+++ classes.java
@@ -0,0 +1,22 @@
+/* **CLASS** 
+A class is a basic building block of an object-oriented language such as java.
+A class is a definition template for structuring and creating objects with the same attributes and methods. An object is called an instance of a
+class as every object created from a class gets its own instances of the variables defined in the class. A class is a definition about objects; the 
+attributes and methods in a class are thus declarations that do not contain values.
+
+in the below program Print is a class.*/
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
+
+/* **METHODS IN A CLASS**
+A method in Java is a set of instructions that can be called for execution using the method name. in the above program main() is a method of class 
+Print.*/
+
+
+
